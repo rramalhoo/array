f71293193c9652be159481fbd47e3726df0c2e1c
rramalhoo <raphael.athenesi@gmail.com>
2025-09-23 13:25:01 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+const carrosPasseio = ['Onix', 'Argo', 'Mobi', 'Nivus']
+
+const carrosSUV=[
+    'T-Cross',
+    'Tracker',
+    'Renegade'
+]
+
+
+const carrosPickUp=[]
+carrosPickUp[0]='RAM'
+carrosPickUp[1]='Toro'
+carrosPickUp[2]='Montana'
