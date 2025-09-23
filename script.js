49532a08085cca30b9a3b2306948fd6f5fa163f8
rramalhoo <raphael.athenesi@gmail.com>
2025-09-23 13:40:32 -0300

    add mais

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
-const carrosPasseio = ['Onix', 'Argo', 'Mobi', 'Nivus']
+const carrosPasseio = ['Onix', 'Argo', 'Mobi', 'Nivus'];
 
 const carrosSUV=[
     'T-Cross',
     'Tracker',
     'Renegade'
-]
+];
 
 
-const carrosPickUp=[]
-carrosPickUp[0]='RAM'
-carrosPickUp[1]='Toro'
-carrosPickUp[2]='Montana'
+const carrosPickUp=[];
+carrosPickUp[0]='RAM';
+carrosPickUp[1]='Toro';
+carrosPickUp[2]='Montana';
+
+const numeros=[5,6,7.8,9.36];
+
+carrosPickUp[1]="Ranger";
+
+meuCarroFavorito=carrosSUV[1];
+alert(meuCarroFavorito);
