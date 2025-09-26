62f5e22c124ba948ffe7a22690ccdd8d0d2c4b8b
rramalhoo <raphael.athenesi@gmail.com>
2025-09-26 08:30:12 -0300

    novas arrays

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//AULA 01
+
 const carrosPasseio = ['Onix', 'Argo', 'Mobi', 'Nivus'];
 
 const carrosSUV=[
@@ -18,3 +20,42 @@ carrosPickUp[1]="Ranger";
 
 meuCarroFavorito=carrosSUV[1];
 alert(meuCarroFavorito);
+
+
+
+//AULA 02
+const frutas=['Banana', 'Laranja', 'Maça', 'Manga'];
+
+
+alert(frutas)
+
+
+/*shift*/
+frutas.shift()
+alert(frutas)
+
+
+/*splice*/
+frutas.splice(2,2,'Limão', 'Kiwi')
+alert(frutas)
+
+
+/*join*/
+alert(frutas.join(' - '))
+
+
+/*concat*/
+const verduras=['Couve', 'Alface']
+const comidas=frutas.concat(verduras)
+alert (comidas)
+
+
+/*slice*/
+const comidasLista2 =['Banana', 'Laranja', 'Maça', 'Manga', 'Couve', 'Alface'];
+const verdura= comida.slice(4)
+alert(comidasLista2)
+
+
+/*sort, reverse*/
+alert(comidas.sort())
+alert(comidas.reverse())
